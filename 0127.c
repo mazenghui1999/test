07d21979f3739085aa43f0bcfb25bd1d9ce0599f
mazenghui1999 <64954738+mazenghui1999@users.noreply.github.com>
2021-01-27 19:29:52 +0800

    Add files via upload

---
+++ 0127.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main(){
+//	int arr[3][4] = {
+//		{ 1, 2, 3, 4 },
+//		{ 5, 6, 7, 8 },
+//		{ 9, 10, 11, 12 }
+//	};
+//	for (int row = 0; row < 3; row++){
+//		for (int j = 0; j < 4; j++){
+//			printf("%d\t", arr[row][j]);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
