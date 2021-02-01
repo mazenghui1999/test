46c11fec074b5f361a82018b81a8723c79dd3d6a
mazenghui1999 <64954738+mazenghui1999@users.noreply.github.com>
2021-02-01 21:45:03 +0800

    一丢丢

---
+++ 0201.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//int Max(int x, int y){
+//	return x > y ? x : y;
+//}
+//int main(){
+//	printf("%d\n", Max(10, 20));
+//	int arr[3][4] = {
+//		{ 1, 2 },
+//		3, 4
+//	};
+//	system("pause");
+//	return 0;
+//}
+
+
+
+////¶ÌÂ·ÇóÖµ
+//int main(){
+//	int i = 0,a =0,b = 2, d = 4;
+//	//i = a++&&++b&&d++;
+//	i = a++ || ++b || d++;
+//	printf(" a=%d\n b=%d\n d=%d\n", a, b, d);
+//	system("pause");
+//	return 0;
+//}
+
+
+//
+//int main(){
+//	printf("%f", (double)10 / 20);
+//	system("pause");
+//	return 0;
+//}
+//int main(){
+//	int n = 30;
+//	if (5 < n &&n < 20){
+//		printf("hehe\n");
+//	}
+//	else{
+//		printf("haha\n");
+//	}
+//	system("pause");
+//	return 0;
+//} 
